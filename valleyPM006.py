9f308c1ebf19535ef041ded4552feecb0dde29f4
RayEscBSU <raymundo.escobedo@u.boisestate.edu>
2024-05-10 08:14:18 -0600

    done, final commit

--- valleyPM006.py
+++ valleyPM006.py
@@ -17,7 +17,13 @@ df = df.iloc[:-2]
 df.columns = new_header
 
 
-#Cleaning Data
+""""
+*** Cleaning Data ***
+replace empty values with 0
+drop unused columns, rename columns
+split dist_num into 2 columns, one column stores the assessor number,
+    the other column stores the district name
+"""
 #replace Nan / NaT withe empty string
 df.fillna(0, inplace =True)
 # Trim leading and trailing whitespaces from all string values
@@ -37,10 +43,26 @@ df = df[df['dist_nam'].str.contains('\d', na=False)]
 
 # Insert new column at position 0 containing the substring of the first three characters from the 'dist_nam' column
 df.insert(0, 'assr_num', df['dist_nam'].str[:3])
-
-# Remove first 4 characters from 'dist_nam' column
+# Check if the first digit of 'dist_nam' column is 0, if so, skip the first digit
+df['assr_num'] = df['assr_num'].apply(lambda x: x[1:] if x.startswith('0') else x)
+#convert assessor number column to int
+df['assr_num'] = df['assr_num'].astype(int)
+#remove digits that are now stored in assr_num col
 df['dist_nam'] = df['dist_nam'].str[4:]
 
-print(df.head())
+""" 
+replace symbols with an empty string from dist_num
+
+Since this data will be used for creating reports I
+will leave these symbols in for ease of reading
+"""
+# # replace # 
+# df['dist_nam'] = df['dist_nam'].str.replace(r'#', '')
+# # replace / 
+# df['dist_nam'] = df['dist_nam'].str.replace(r'/', '')
+# # replace . 
+# df['dist_nam'] = df['dist_nam'].str.replace(r'.', '')
+
+#print(df.head())
 #create a csv with results from changes
 df.to_excel(r"L:\RayE\2024_County_Reports\PM006-Final_TaxPolicy_Cleaned.xlsx",sheet_name='Sheet1', index=False, header=True, engine='openpyxl')
