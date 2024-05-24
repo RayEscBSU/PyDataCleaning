ecbedf29d6760df70d8cedbd5f82e272313d1f38
RayEscBSU <raymundo.escobedo@u.boisestate.edu>
2024-05-24 08:14:40 -0600

    added column mapping and code to push to ssms

--- Ada003Update.py
+++ Ada003Update.py
@@ -3,13 +3,6 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import pandas as pd
 
-df = pd.read_excel(r"C:\Users\rescobedo\OneDrive - State of Idaho\RayE\2024_County_Reports\Ada County (003) - Copy.xlsx")
-
-# Trim leading and trailing whitespaces from all string values
-df = df.applymap(lambda x: x.strip() if isinstance(x, str) else x)
-# Trim leading and trailing whitespaces from the headers
-df.columns = df.columns.str.strip()
-
 # Define existing SQL table model
 Base = declarative_base()
 
@@ -44,11 +37,67 @@ class CountyCategoryDetail(Base):
     mining = Column('Mining', Integer, default=0)
     id = Column(Integer, primary_key=True)
 
+# Create session and engine - Database connection details
+db_connection_string = 'mssql+pyodbc://@TAXDB-PT001:1433/Budget_Levey_Data?driver=ODBC+Driver+17+for+SQL+Server&trusted_connection=yes'
+engine = create_engine(db_connection_string)
+Session = sessionmaker(bind=engine)
+session = Session()
+
+#read excel file
+df = pd.read_excel(r"C:\Users\rescobedo\OneDrive - State of Idaho\RayE\2024_County_Reports\Ada County (003) - Copy.xlsx")
+
+# Trim leading and trailing whitespaces from all string values
+df = df.applymap(lambda x: x.strip() if isinstance(x, str) else x)
+# Trim leading and trailing whitespaces from the headers
+df.columns = df.columns.str.strip()
+
+"""
+    The column mapping will specify which columns from the CSV I want to read
+    all others will be ignored.  
+"""
+# Define the column mapping: DataFrame column -> SQLAlchemy attribute
+column_mapping = {
+    'Category': 'category',
+    'Acres': 'acreage',
+    'Market Value': 'market_value',
+    'Homeowners' : 'homeowner_s_exemption',
+    'Increment' : 'increment',
+    'Hardship' : 'hardship',
+    'QIE' : 'qie',
+    'Pollution' : 'pollution',
+    'New Capital Investment' : 'new_cap_investment', 
+    'Casualty' : 'casualty_loss',
+    'Site Improvments' : 'site_improvement',
+    'PP Exemption' : 'personal_property_exemption',
+    'Taxable Value' : 'net_value'
+}
+
+# Rename DataFrame columns based on mapping and keep only those columns
+df = df.rename(columns=column_mapping).loc[:, column_mapping.values()]
+
+"""
+    Columns that exist in SQl database but not found in column mapping
+    will be updated with the defualt value set above   
+ """
+# get columns and default values from sqlalchemy table
+for column, default_value in CountyCategoryDetail.__table__.columns.items():
+    # check if columns is mapped and but not found in csv 
+    if column in column_mapping.values() and column not in df:
+        # set default value it one is assigned, if no default is assigned set to none
+        df[column] = default_value.default.arg if default_value.default is not None else None 
+
+# Convert DataFrame rows to SQLAlchemy objects
+county_details = [CountyCategoryDetail(**record) for record in df.to_dict(orient='records')]
+
+# Add new records to the database
+session.add_all(county_details)
+session.commit()
+session.close()
 
 
 
 #print
-print(df.iloc[:10, :10])
+#print(df.iloc[:10, :10])
 
 #create a csv with results from changes
-df.to_excel(r"C:\Users\rescobedo\OneDrive - State of Idaho\RayE\2024_County_Reports\Ada County (003) TestOutput.xlsx",sheet_name='Sheet1', index=False, header=True, engine='openpyxl')
+#df.to_excel(r"C:\Users\rescobedo\OneDrive - State of Idaho\RayE\2024_County_Reports\Ada County (003) TestOutput.xlsx",sheet_name='Sheet1', index=False, header=True, engine='openpyxl')
